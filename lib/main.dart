1308371566ce177ecd6247fcf33fe16d9345449c
Emmanuel Chargoy <charger513@hotmail.com>
2021-08-13 13:15:50 -0500

    Se arregla el brightness del status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_stripe_app/pages/pago_completo_page.dart';
 
 import 'pages/home_page.dart';
@@ -8,10 +9,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ///! Se pone el status bar brightness en dark
+    ///! Para cuando no hay appBar en algún page
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'StripeApp',
-      initialRoute: 'home',
+      initialRoute: 'pago_completo',
       routes: {
         'home': (_) => HomePage(),
         'pago_completo': (_) => PagoCompletoPage(),
@@ -19,6 +29,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         primaryColor: const Color(0xff284879),
         scaffoldBackgroundColor: const Color(0xff21232A),
+
+        ///! Se pone el status bar brightness en dark
+        ///! porque el appbar sobreescribe el de SystemChrome ^^
+        appBarTheme:
+            Theme.of(context).appBarTheme.copyWith(brightness: Brightness.dark),
       ),
     );
   }
