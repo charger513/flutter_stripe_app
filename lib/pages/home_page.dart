be1c99b063491279e1b38f74088351a4008848b3
Emmanuel Chargoy <charger513@hotmail.com>
2021-08-13 11:31:40 -0500

    Diseño de la pantalla Home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:flutter_stripe_app/data/tarjetas.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagar'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('HomePage'),
+      body: Stack(
+        children: [
+          Positioned(
+            width: size.width,
+            height: size.height,
+            top: 200,
+            child: PageView.builder(
+              controller: PageController(viewportFraction: 0.9),
+              physics: BouncingScrollPhysics(),
+              itemCount: tarjetas.length,
+              itemBuilder: (_, i) {
+                final tarjeta = tarjetas[i];
+                return CreditCardWidget(
+                  cardNumber: tarjeta.cardNumber,
+                  expiryDate: tarjeta.expiracyDate,
+                  cardHolderName: tarjeta.cardHolderName,
+                  cvvCode: tarjeta.cvv,
+                  showBackView: false,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
